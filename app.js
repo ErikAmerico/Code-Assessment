2cda1f51dd9a592a718b2ae7926765ca75816064
Erik Olson <111093132+ErikAmerico@users.noreply.github.com>
2023-04-26 06:37:36 -0400

    shuffled questions. added countdown timer

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ header.setAttribute("style", "text-align:center; ")
 choicesContainer.setAttribute("style", "text-align:center; ")
 
 
-const questions = [{
+const boringQuestions = [{
     question: "What is 2+2?",
     answers: ["3", "4", "5", "5"],
     correctAnswer: "4" //this.answers[1]
@@ -52,6 +52,19 @@ function updateQuestion() {
     choicesContainer.innerHTML = "";
     renderChoices();
 }
+
+
+
+//Again, I don't fully understand this function.
+//But the game would be even more boring without it.
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+const questions = shuffleArray(boringQuestions);
 
 startButton.addEventListener('click', function () {
     console.log("game started")
@@ -84,4 +97,21 @@ function renderChoices() {
     //**console.log(choices);
     //*console.log(selectedValues)
     //**return correct;
+}
+
+let countdownInterval;
+let countdownTime = 90;
+
+function startCountdown() {
+    countdownInterval = setInterval(function () {
+        countdownTime--;
+        if (countdownTime <= 0) {
+            clearInterval(countdownInterval);
+            countdownTimer.textContent = 0.0;
+            countdownTime = 90;
+            endGame();
+        } else {
+            countdownTimer.textContent = countdownTime;
+        }
+    }, 1000);
 }
