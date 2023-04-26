4ec119006e4ea9aff6d9885055ed02f63015dd90
Erik Olson <111093132+ErikAmerico@users.noreply.github.com>
2023-04-26 00:04:36 -0400

    removed ul/li styling via CSS file. Rendered multiple choice answers to page on start button click

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const startButton = document.querySelector('.start');
 const submitButton = document.querySelector('.submit')
 const questionH2 = document.querySelector('.question');
 const choicesContainer = document.querySelector('.answers');
-const result = document.querySelector('.result')
-const h1 = document.querySelector('h1')
-const header = document.querySelector('header')
-const main = document.querySelector('main')
-const body = document.querySelector('body')
+const result = document.querySelector('.result');
+const h1 = document.querySelector('h1');
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+const body = document.querySelector('body');
+
 
 body.style.backgroundColor = 'lightgrey'
 startButton.setAttribute("style", "background-color: green; border-radius: 10px;")
@@ -49,6 +50,7 @@ function updateQuestion() {
     const currentQuestion = questions[CurrentQuestionsIndex]; //where is CQI
     questionH2.textContent = currentQuestion.question;
     choicesContainer.innerHTML = "";
+    renderChoices();
 }
 
 startButton.addEventListener('click', function () {
@@ -59,3 +61,27 @@ startButton.addEventListener('click', function () {
     updateQuestion();
 })
 
+//selectedValues is an attempt to collect chosen answer.
+function renderChoices() {
+    const currentQuestion = questions[CurrentQuestionsIndex];
+    const selectedValues = [];
+    for (let i = 0; i < currentQuestion.answers.length; i++) {
+        const answer = currentQuestion.answers[i];
+        const li = document.createElement("li");
+        const input = document.createElement("input");
+        input.type = "radio";
+        input.name = "q1"
+        input.value = answer;
+        input.checked = false;
+        li.appendChild(input);
+        li.appendChild(document.createTextNode(answer));
+        li.addEventListener('click', function () {
+            selectedValues.push(input.value);
+        })
+        choicesContainer.appendChild(li)
+    }
+    const choices = document.querySelectorAll('input[name="q1"]');
+    //**console.log(choices);
+    //*console.log(selectedValues)
+    //**return correct;
+}
