36c7cab61c509b210df33a260c83d5143f72ca84
Erik Olson <111093132+ErikAmerico@users.noreply.github.com>
2023-04-25 23:41:29 -0400

    initial commits. added elements to html. Created questions and selected html elements in JS. began styling restart button

---
+++ app.js
@@ -0,0 +1,40 @@
+
+const startButton = document.querySelector('.start');
+const submitButton = document.querySelector('.submit')
+const questionH2 = document.querySelector('.question');
+const choicesContainer = document.querySelector('.answers');
+const result = document.querySelector('.result')
+const h1 = document.querySelector('h1')
+const header = document.querySelector('header')
+const main = document.querySelector('main')
+const body = document.querySelector('body')
+
+const restartButton = document.createElement('button');
+restartButton.textContent = 'RESTART';
+restartButton.classList.add('restartButton');
+startButton.insertAdjacentElement('afterend', restartButton)
+restartButton.setAttribute("style", "background-color: yellow; border-radius: 10px;")
+
+
+
+
+const questions = [{
+    question: "What is 2+2?",
+    answers: ["3", "4", "5", "5"],
+    correctAnswer: "4" //this.answers[1]
+},
+{
+    question: "What is the capital of Mass?",
+    answers: ["Amesbury", "Boston", "Derry, NH", "Somerville"],
+    correctAnswer: "Boston"
+},
+{
+    question: "Who is the President?",
+    answers: ["Obama", "Trump", "Biden", "Tom Brady"],
+    correctAnswer: "Biden"
+},
+{
+    question: "What month is it?",
+    answers: ["Tuesday", "March", "December", "April"],
+    correctAnswer: "April"
+}];
